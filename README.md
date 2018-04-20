68426b0e1855c267a2e5fc17fc3b69b7d33b1cac
FranckAchard <36164145+FranckAchard@users.noreply.github.com>
2018-04-20 12:05:20 +0200

    sav

--- README.md
+++ README.md
@@ -3,15 +3,17 @@
 
 ### Description :
 
-...
+Cette extension permet d'afficher la mention demandée par Simplon dans chaque article de mon blog http://9b018b9657cc467f9e0603596b7e7bd8.testing-url.ws
 
 ### Installation :
 
-...
+1. Faire un zip contenant les fichiers partenariat-simplon.php et README.md
+2. Dans WordPress, aller dans le menu d'ajout d'extension et téléverser le fichier zip
+3. activer le plugin 
 
 ### Utilisation :
 
-...
+Dans les articles, ajouter le shortcode "[simplon]" dans une zone de texte pour afficher la mention
 
 ### Ressources :
 #### Déclaration du plugin :
--- partenariat-simplon.php
+++ partenariat-simplon.php
@@ -1,4 +1,17 @@
 <?php
 /*
-
+Plugin Name: Plugin Partenariat Simplon
+Plugin URI: https://github.com/FranckAchard/partenariat-simplon
+Description: Extension permettant d'ajouter la notion demandée par Simplon
+Version: 0.1
+Author: Franck ACHARD
+Author URI: https://github.com/FranckAchard
+License: GPL2
 */
+
+function PartenariatSimplon() {
+	return '<p><i>La publication de cet article est possible grâce à mon super partenaire <a href="https://simplon.co/" target=_blank>simplon.co</a> - une entreprise de
+    l’économie sociale et solidaire et un réseau de « fabriques » (écoles) qui propose
+    des formations GRATUITES pour devenir développeur web.</i></p>';
+}
+add_shortcode('simplon', 'PartenariatSimplon');
